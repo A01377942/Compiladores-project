e8cca0738fcd49277e6fa6b0d8d23a07ae120176
A01377942 <a01377942@tec.mx>
2022-03-20 23:50:59 -0600

    Token Category Created for Quetzal Lenguage

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Compiladores-project
-Espacio para realizar el proyecto de Compiladores
+# COMPILADORES-PROJECT
+
+# Quetzal Dragon Compiler
+# Versión: 0.1
+
+This program is free software. You may redistribute it under the terms of the GNU General Public License version 3 or later. See `license.txt` for details.
+
+Included in this release:
+
+* Lexical analysis
+
+## How to Build
+
+At the terminal type:
+
+    make
+
+## TODO
+Agregar secciíon "HOW TO RUN"
+
+
+## How to Clean
+
+To delete all the files that get created automatically, at the terminal type:
+
+    make clean
---
+++ TokenCategory.cs
@@ -0,0 +1,49 @@
+/*
+    
+*/
+
+namespace QuetzalDragon{
+
+    enum TokenCategory{
+        STRING,
+        CHARACTER,
+        AND,
+        OR,
+        NOT,
+        ASSIGN,
+        ELSE,
+        TRUE,
+        FALSE,
+        EOF,
+        IDENTIFIER,
+        IF,
+        ELIF,
+        ELSE,
+        BREAK,
+        RETURN,
+        END_OF_LINE,
+        INC,
+        DEC,
+        LOOP,
+        INT_LITERAL,
+        LEFT_SQUARE_BRACKET,
+        RIGHT_SQUARE_BRACKET,
+        LEFT_CURLY_BRACE,
+        RIGHT_CURLY_BRACE,
+        PLUS,
+        LESS,
+        MULTIPLICATION,
+        DIVISION,
+        REMINDER,
+        COMA,
+        EQUAL_TO,
+        NOT_EQUAL_TO,
+        GREATHER_THAN,
+        LESS_THAN,
+        GREATHER_EQUAL_THAN,
+        LESS_EQUAL_THAN,
+        COMMENT,
+        MULTI_LINE_COMMENT,
+    }
+
+} 
